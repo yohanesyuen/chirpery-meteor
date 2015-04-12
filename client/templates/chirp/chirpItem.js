82f4cd0a231385b684a35539e8d512f67272c539
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-04-12 16:00:29 +0800

    Fixes #1, refactor parsing code for mentions

--- client/templates/chirp/chirpItem.js
+++ client/templates/chirp/chirpItem.js
@@ -1,3 +1,12 @@
+var linkToMention = function(entity, text, options) {
+  var at = text.substring(entity.indices[0], entity.indices[0] + 1);
+  var user = entity.screenName;
+
+  var displayScreenName = text.substring(entity.indices[0], entity.indices[1]);
+
+  return '<a href="/' + user + '">' + displayScreenName + '</a>';
+};
+
 Template.chirpItem.helpers({
   prettyCreationDate: function () {
     console.log(this.createdAt);
@@ -5,19 +14,22 @@ Template.chirpItem.helpers({
   },
 
   formattedChirpBody: function () {
-    var formattedBody = this.body;
-    var mentions = this.userMentions;
+    var text = this.body;
+    var formattedBody = "";
+    var entities = this.entities;
 
-    for (i in mentions) {
-      var mention = mentions[i],
-          mentionStartIndex = mention.indices[0],
-          mentionEndIndex = mention.indices[1];
-      var leftHTML = formattedBody.substring(0, mentionStartIndex);
-      var rightHTML = formattedBody.substring(mentionEndIndex);
-      var mentionString = formattedBody.substring(mentionStartIndex, mentionEndIndex);
-      console.log(mention);
-      var mentionHTML = '<a href="/' + mention.screenName + '">' + mentionString + '</a>'
-      formattedBody = leftHTML + mentionHTML + rightHTML;
+    // TODO: Possibly create a package for this to be uploaded to the atmosphere?
+    // TODO: refactor these functionalities to another library? They do not belong here
+    // TODO: extract all entities into an array and work with them as such
+    var userMentions = entities.userMentions; // This is an ugly way of extracting
+
+    var startIndex = 0;
+
+    for (i in userMentions) {
+      var mention = userMentions[i];
+      formattedBody += text.substring(startIndex, mention.indices[0]);
+      formattedBody += linkToMention(mention, text);
+      startIndex = mention.indices[1];
     }
 
     return formattedBody;
--- server/methods/chirps.js
+++ server/methods/chirps.js
@@ -1,46 +1,58 @@
+
+var extractMentionEntitiesFromText = function (text, opts) {
+  var mentionRegEx = /(@[a-zA-Z0-9]{1,20})/g;
+
+  mentionsMatch = mentionRegEx.exec(text);
+
+  var mentions = [];
+
+  while (mentionsMatch) {
+    var matchedString = mentionsMatch[1];
+    var mentionStartIndex = mentionsMatch.index;
+    var mentionEndIndex = mentionStartIndex + matchedString.length;
+
+    var username = matchedString.substring(1);
+
+    var mentionedUser = Meteor.users.findOne({username : username});
+
+    var mention = {
+      indices : [mentionStartIndex, mentionEndIndex],
+      screenName : mentionedUser.username,
+      id : mentionedUser._id,
+    };
+
+    mentions.push(mention);
+    mentionsMatch = mentionsRegEx.exec(text);
+
+  }
+
+  return mentions;
+};
+
 Meteor.methods({
   'chirps.create' : function(data) {
+
+    var body = data.body;
+
     if (!Meteor.userId()) {
       throw new Meteor.error('not-authorized');
     }
 
-    if (data.body.length > 140) {
+    if (body.length > 140) {
       throw new Meteor.error('Too many characters!');
     }
 
-    var mentionsRegEx = /(@[a-zA-Z0-9]{1,20})/g;
+    var entities = {};
 
-    var mentions = [];
-
-    mentionsMatch = mentionsRegEx.exec(data.body);
-
-    while (mentionsMatch) {
-      var matchedString = mentionsMatch[1];
-      var mentionStartIndex = mentionsMatch.index;
-      var mentionEndIndex = mentionStartIndex + matchedString.length;
-
-      var username = matchedString.substring(1);
-
-      var mentionedUser = Meteor.users.findOne({username : username});
-
-      var mention = {
-        indices : [mentionStartIndex, mentionEndIndex],
-        screenName : mentionedUser.username,
-        id : mentionedUser._id,
-      };
-
-      mentions.push(mention);
-      mentionsMatch = mentionsRegEx.exec(data.body);
-
-    }
+    entities.userMentions = extractMentionEntitiesFromText(body);
 
     console.log(JSON.stringify(mentions));
 
     Chirps.insert({
       owner: Meteor.userId(),
       username: Meteor.user().username,
-      body: data.body,
-      userMentions : mentions,
+      body: body,
+      entities : entities,
       createdAt: new Date()
     });
   }
