f0315d7cbab8808327d68b44f3f0b3c10a2f2be1
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-04-12 16:45:57 +0800

    Refactor link to mentions as a function

--- client/templates/chirp/chirpItem.js
+++ client/templates/chirp/chirpItem.js
@@ -7,6 +7,22 @@ var linkToMention = function(entity, text, options) {
   return '<a href="/' + user + '">' + displayScreenName + '</a>';
 };
 
+var autoLinkMentions = function(text, entities, options) {
+  var userMentions = entities.userMentions;
+
+  var startIndex = 0;
+  var result = "";
+
+  for (in in userMentions) {
+    var mention = userMentions[i];
+    result += text.substring(startIndex, mention.indices[0]);
+    result += linkToMention(mention, text);
+    startIndex = mention.indices[1];
+  }
+
+  return result;
+};
+
 Template.chirpItem.helpers({
   prettyCreationDate: function () {
     console.log(this.createdAt);
@@ -21,16 +37,8 @@ Template.chirpItem.helpers({
     // TODO: Possibly create a package for this to be uploaded to the atmosphere?
     // TODO: refactor these functionalities to another library? They do not belong here
     // TODO: extract all entities into an array and work with them as such
-    var userMentions = entities.userMentions; // This is an ugly way of extracting
 
-    var startIndex = 0;
-
-    for (i in userMentions) {
-      var mention = userMentions[i];
-      formattedBody += text.substring(startIndex, mention.indices[0]);
-      formattedBody += linkToMention(mention, text);
-      startIndex = mention.indices[1];
-    }
+    formattedBody = autoLinkMentions(text, entities);
 
     return formattedBody;
   }
