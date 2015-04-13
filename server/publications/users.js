4716cc6ee38309e098ca63193728b9ffe353bc26
Yohanes Yuen <yuen.wei.ping@gmail.com>
2015-04-14 03:40:03 +0800

    Added filter for user pub to only publish required info

--- server/publications/users.js
+++ server/publications/users.js
@@ -1,3 +1,9 @@
 Meteor.publish('users', function () {
-  return Meteor.users.find({});
+  return Meteor.users.find({
+    fields : {
+      username : 1,
+      _id : 1,
+      createdAt : 1
+    }
+  });
 });
